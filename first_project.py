45590a37c55b7e69cb3b02764a18cdca358660fc
avadhutjadhav1294 <avadhut.jadhav1294@gmail.com>
2023-06-12 14:24:51 +0530

    added home_name home_name_age functions

--- first_project.py
+++ first_project.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/home/') #decorator drfines the   -- called controller function
 def home():  
-    return "hello, this is our first flask website";  
-  
+    return "hello, this is our first flask website"  
+
+@app.route('/home/<name>/') #decorator drfines the   -- called controller function
+def home_name(name):  
+    return f"hello,{name} this is our first flask website" 
+
+@app.route('/home/<name>/<int:age>') #decorator drfines the   -- called controller function
+def home_name_age(name,age):  
+    return f"hello,{name} : {age} this is our first flask website" 
+
 if __name__ =='__main__':  
     app.run(debug = True)  
